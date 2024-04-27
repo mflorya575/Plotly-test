11907c451099e6ab0baf5f63cd68cb496c60d14a
Michael Florya <mih2303@mail.ru>
2024-04-28 01:14:31 +0300

    create first type of graph

--- django_plotly/core/urls.py
+++ django_plotly/core/urls.py
@@ -1,1 +1,9 @@
+from django.urls import path
+from . import views
 
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', views.chart, name='chart'),
+]
--- django_plotly/core/views.py
+++ django_plotly/core/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+import plotly.express as px
 
-# Create your views here.
+from core.models import CO2
+from core.forms import DateForm
+
+
+def chart(request):
+    co2 = CO2.objects.all()
+    start = request.GET.get('start')
+    end = request.GET.get('end')
+
+    if start:
+        co2 = co2.filter(date__gte=start)
+    if end:
+        co2 = co2.filter(date__lte=end)
+
+    fig = px.line(
+        x=[c.date for c in co2],
+        y=[c.average for c in co2],
+        title='CO2 PPM',
+        labels={'x': 'Date', 'y': 'CO2 PPM'}
+    )
+
+    fig.update_layout(title={
+        'font_size': 22,
+        'xanchor': 'center',
+        'x': 0.5
+    })
+
+    chart = fig.to_html()
+
+    context = {'chart': chart, 'form': DateForm()}
+    return render(request, 'core/chart.html', context)
